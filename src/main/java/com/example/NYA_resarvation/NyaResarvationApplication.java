ca94c4ab105d2383481e4de813b5e903d11c9c0c
sato_issei <sato.issei@alhinc.jp>
2025-10-21 09:28:03 +0900

    共有用作業①

---
+++ src/main/java/com/example/NYA_resarvation/NyaResarvationApplication.java
@@ -0,0 +1,13 @@
+package com.example.NYA_resarvation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyaResarvationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NyaResarvationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NYA_resarvation/NyaResarvationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NYA_resarvation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyaResarvationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
