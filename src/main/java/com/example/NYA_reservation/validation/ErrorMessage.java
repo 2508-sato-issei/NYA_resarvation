c3a47ae18fcde9fe1a839fe957799396780017e0
sato_issei <sato.issei@alhinc.jp>
2025-10-21 11:06:22 +0900

    エラーメッセージ用のクラスを作成

---
+++ src/main/java/com/example/NYA_reservation/validation/ErrorMessage.java
@@ -0,0 +1,43 @@
+package com.example.NYA_reservation.validation;
+
+public class ErrorMessage {
+
+    public static final String E0001 = "アカウントを入力してください";
+    public static final String E0002 = "パスワードを入力してください";
+    public static final String E0003 = "ログインに失敗しました";
+    public static final String E0004 = "予約日を入力してください";
+    public static final String E0005 = "予約時間を入力してください";
+    public static final String E0006 = "予約人数を入力してください";
+    public static final String E0007 = "選択された日付は予約できません";
+    public static final String E0008 = "入力された時間は予約できません";
+    public static final String E0009 = "定員を超えています";
+    public static final String E0010 = "無効なアクセスです";
+    public static final String E0011 = "不正なパラメータが入力されました";
+    public static final String E0012 = "氏名を入力してください";
+    public static final String E0013 = "アカウントは10文字以下で入力してください";
+    public static final String E0014 = "パスワードは半角文字かつ6文字以上20文字以下で入力してください";
+    public static final String E0015 = "氏名は10文字以下で入力してください";
+    public static final String E0016 = "アカウントが重複しています";
+    public static final String E0017 = "パスワードと確認用パスワードが一致しません";
+    public static final String E0018 = "店舗名を入力してください";
+    public static final String E0019 = "電話番号を入力してください";
+    public static final String E0020 = "住所を入力してください";
+    public static final String E0021 = "エリアを選択してください";
+    public static final String E0022 = "ジャンルを選択してください";
+    public static final String E0023 = "説明を入力してください";
+    public static final String E0024 = "営業開始時間を入力してください";
+    public static final String E0025 = "営業終了時間を入力してください";
+    public static final String E0026 = "無効な定員を入力してくださいアクセスです";
+    public static final String E0027 = "最低利用料金を入力してください";
+    public static final String E0028 = "最高利用料金を入力してください";
+    public static final String E0029 = "店舗名は50文字以下で入力してください";
+    public static final String E0030 = "説明は500文字以下で入力してください";
+    public static final String E0031 = "正しい電話番号形式で入力してください。";
+    public static final String E0032 = "入力された時間は予約できません";
+    public static final String E0033 = "ログインしてください";
+
+    // インスタンス化防止
+    private ErrorMessage() {
+    }
+
+}
