f358bd1fdd51cb73c8951fe2245424a219799c8a
sato_issei <sato.issei@alhinc.jp>
2025-10-22 18:41:15 +0900

    UserConverterを修正

--- src/main/java/com/example/NYA_reservation/converter/UserConverter.java
+++ src/main/java/com/example/NYA_reservation/converter/UserConverter.java
@@ -4,6 +4,9 @@ import com.example.NYA_reservation.controller.form.UserForm;
 import com.example.NYA_reservation.repository.entity.User;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class UserConverter {
 
@@ -19,6 +22,24 @@ public class UserConverter {
         userForm.setCreatedDate(user.getCreatedDate());
         userForm.setUpdatedDate(user.getUpdatedDate());
         return userForm;
+    }
+    // List<User> → List<UserForm>
+    public List<UserForm> toUserFormList(List<User> userList) {
+        List<UserForm> users = new ArrayList<>();
+
+        for (User result : userList) {
+            UserForm user = new UserForm();
+            user.setId(result.getId());
+            user.setAccount(result.getAccount());
+            user.setPassword(result.getPassword());
+            user.setName(result.getName());
+            user.setAuthority(result.getAuthority());
+            user.setStopped(result.isStopped());
+            user.setCreatedDate(result.getCreatedDate());
+            user.setUpdatedDate(result.getUpdatedDate());
+            users.add(user);
+        }
+        return users;
     }
 
 }
--- src/main/java/com/example/NYA_reservation/service/UserService.java
+++ src/main/java/com/example/NYA_reservation/service/UserService.java
@@ -9,11 +9,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.List;
-
-import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -92,34 +89,15 @@ public class UserService {
     }
 
     //ユーザー情報全件取得
-    public List<UserForm> findAllUser(){
+    public List<UserForm> findAllUser() {
         List<User> results = userRepository.findAll();
-        return setUserForm(results);
+        return userConverter.toUserFormList(results);
     }
 
     //ユーザー停止・有効切り替え
-    public void changeIsStopped(Integer id, boolean isStopped){
+    public void changeIsStopped(Integer id, boolean isStopped) {
         Timestamp ts = new Timestamp(System.currentTimeMillis());
         userRepository.changeIsStopped(id, isStopped, ts);
-    }
-
-
-    private List<UserForm> setUserForm(List<User> results){
-        List<UserForm> users = new ArrayList<>();
-
-        for(User result : results){
-            UserForm user = new UserForm();
-            user.setId(result.getId());
-            user.setAccount(result.getAccount());
-            user.setPassword(result.getPassword());
-            user.setName(result.getName());
-            user.setAuthority(result.getAuthority());
-            user.setStopped(result.isStopped());
-            user.setCreatedDate(result.getCreatedDate());
-            user.setUpdatedDate(result.getUpdatedDate());
-            users.add(user);
-        }
-        return users;
     }
 
 }
