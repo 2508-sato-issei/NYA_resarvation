63ecfae49348fd5bed5ef274275d5daefd195b51
taguchi.keita <taguchi.keita@alhinc.jp>
2025-10-24 14:45:52 +0900

    レイアウト調整

--- src/main/java/com/example/NYA_reservation/config/SecurityConfig.java
+++ src/main/java/com/example/NYA_reservation/config/SecurityConfig.java
@@ -44,8 +44,8 @@ public class SecurityConfig {
                         .clearAuthentication(true)
                 )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/login", "/user/new", "/search", "/restaurant/**").permitAll()
-                        .requestMatchers("/css/**", "/js/**", "/storage/**").permitAll()
+                        .requestMatchers("/", "/login", "/user/new", "/user/add", "/search", "/restaurant/**").permitAll()
+                        .requestMatchers("/webjars/**", "/css/**", "/js/**", "/storage/**").permitAll()
                         .requestMatchers("/mypage/**", "/reservation/**", "/user/edit/**").hasRole("USER")
                         .requestMatchers("/admin/**").hasRole("ADMIN")
 //                        .requestMatchers(HttpMethod.POST, "/**").authenticated()
